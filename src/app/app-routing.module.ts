36df3c20132e3212ed7e080c362e6bed857e308b
Edmont05 <montanoeddy1120@gmail.com>
2022-04-12 22:33:28 -0400

    MC-Admin-Interfaces
    Interfaces to admin, example:
    add topic, classroom or teacher

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authentication/components/login/login.component';
+import { AddClassroomComponent } from './classroom/add-classroom/add-classroom.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AddTeacherComponent } from './teacher/add-teacher/add-teacher.component';
+import { AddTopicComponent } from './topic/add-topic/add-topic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: LoginComponent},
+  {path: 'menu',component: MenuComponent},
+  {path: 'add-teacher',component: AddTeacherComponent},
+  {path: 'add-classroom',component: AddClassroomComponent},
+  {path: 'add-topic',component: AddTopicComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClassroomModule } from './classroom/classroom.module';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SidebarModule } from './sidebar/sidebar.module';
+import { TeacherModule } from './teacher/teacher.module';
+import { TopicModule } from './topic/topic.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TeacherModule,
+    TopicModule,
+    ClassroomModule,
+    SidebarModule
   ],
   providers: [],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
